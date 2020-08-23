cab6349ae2aa0899b215e873999914372d4c655e
yh72167 <yh.19972372@gmail.com>
2020-08-23 17:56:17 +0900

    [add]最初のコミット

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link type="text/css" rel="stylesheet" href="./css/reset.css"> <!-- 順番を間違えないこと -->
+    <link type="text/css" rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    <header>
+    ヘッダー
+    </header>
+    <main>
+        <article class="main-screen">
+            メイン画面
+        </article>
+        <aside class="operation">
+            <div class="figure-wrap">
+                <lavel class="figure-panel">
+                    <img src="img/figure_1.jpg">
+                </lavel>
+                <lavel class="figure-panel">
+                    <img src="img/figure_2.jpg">
+                </lavel>
+                <lavel class="figure-panel">
+                    <img src="img/figure_3.jpg">
+                </lavel>
+                <lavel class="figure-panel">
+                    <img src="img/figure_4.jpg">
+                </lavel>
+            </div>
+        </aside>
+        <aside class="contribution">
+            貢献度
+        </aside>
+    </main>
+    <footer>
+    フッター
+    </footer>
+</body>
+</html>
